1c54948fbafc3b0d94d9b3f2885323e1efb9fa31
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2022-08-06 05:54:52 -0300

    :rocket: History Page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,47 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export const History = () => {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status status="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status status="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status status="yellow">Em andamento</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,3 +1,88 @@
 import styled from 'styled-components'
 
-export const HistoryContainer = styled.header``
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.colors['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 500px;
+
+    th {
+      background-color: ${({ theme }) => theme.colors['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${({ theme }) => theme.colors['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-left: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${({ theme }) => theme.colors['gray-700']};
+      border-top: 4px solid ${({ theme }) => theme.colors['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        width: 50%;
+      }
+
+      &:last-child {
+        padding-left: 1.5rem;
+      }
+    }
+  }
+`
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  status: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 0.5rem;
+    height: 0.5rem;
+    background-color: ${({ theme, status }) =>
+      theme.colors[STATUS_COLORS[status]]};
+    border-radius: 50%;
+  }
+`
